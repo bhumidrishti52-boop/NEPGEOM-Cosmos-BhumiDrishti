cd08d7fecfe5bf9c2d9295ae88ea0dc181055e85
Bishal BK <230144bishal@cosmoscollege.edu.np>
2026-05-31 18:14:05 +0545

    define MapTiler key, GEE region bounds and map center coordinates

---
+++ Frontend/js/map.js
@@ -0,0 +1,14 @@
+// Mapbox / MapLibre Key
+const MAPTILER_KEY = 'VSrXKSaprVudRxsjfdAF';
+
+// Define exact GEE region bounds
+const bounds = [
+    [86.9816852324489, 26.572226888007714], // Southwest
+    [87.34088811330828, 26.849841970232982] // Northeast
+];
+
+// Center of the map
+const center = [
+    (86.9916852324489 + 87.33088811330828) / 2,
+    (26.582226888007714 + 26.839841970232982) / 2
+];
