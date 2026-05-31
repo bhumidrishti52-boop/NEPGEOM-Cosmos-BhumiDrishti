f2ab6742994934ef82bfd83d4dd18745491bcb7c
RoniDM011 <dangolronish2@gmail.com>
2026-06-01 00:57:30 +0545

    added EE init and class structure

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from typing import Optional
+import os
+import ee
+from fastapi.staticfiles import StaticFiles
 
 app=FastAPI()
 
@@ -11,3 +15,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+try:
+    ee.Initialize(project='flood-482116')
+    print("Earth Engine Initialized Successfully")
+except Exception as e:
+    print(f"Auth Error: {e}")
+
+class ChatRequest(BaseModel):
+    question: str
+    land_data: dict
+
+
+class ReportRequest(BaseModel):
+    analysis_data: dict
