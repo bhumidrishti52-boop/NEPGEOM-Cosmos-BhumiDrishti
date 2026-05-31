b7373783b9ba82a196afda131f702c06349762c4
RoniDM011 <dangolronish2@gmail.com>
2026-06-01 03:05:31 +0545

    init import reportlab lib

---
+++ Backend/pdf_reprt.py
@@ -0,0 +1,10 @@
+from reportlab.lib.pagesizes import A4
+from reportlab.lib.units import mm
+from reportlab.lib.colors import HexColor
+from reportlab.platypus import (
+    SimpleDocTemplate, Paragraph, Spacer, Table, TableStyle, HRFlowable
+)
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_CENTER, TA_LEFT
+import io
+from datetime import datetime
